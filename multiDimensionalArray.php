3066ee839e3913ba5e0c1247eed7977c9a0d22a7
devriazul <riazul56@gmail.com>
2021-09-01 22:13:57 +0600

    MultiDimentional  array

---
+++ multiDimensionalArray.php
@@ -0,0 +1,16 @@
+<?php
+
+$array = [
+    'name' => "Riazul Islam",
+    'age' => "28",
+    'address' => [
+        'street_address' => 'House 203/204',
+        'city' => 'Dhaka',
+        'country' => 'Bangladesh',
+    ], 'mobile' => [
+        'country_prefix' => '+880',
+        'naional_number' => '1722754100'
+    ]
+];
+
+echo $array['name'];
