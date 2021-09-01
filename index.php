93de3b8882bb67c2433477deac98d3d14165c6e3
devriazul <riazul56@gmail.com>
2021-09-01 21:19:12 +0600

    If Else

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
+$day = 'Friday';
+$today = 'Saturday';
 
-$name = 'Riazul';
-$name .= ' Islam';
-
-echo $name;
-
-echo '<br/>';
-
-//comparison
-
-var_dump(10 <= 11);
-echo '<br/>';
-
-var_dump(10 == 9);
-echo '<br/>';
-
-var_dump(10 != 9);
-echo '<br/>';
-
-//data type check and value check
-
-var_dump(10 === '10');
-echo '<br/>';
-
-var_dump(10 == '10');
-echo '<br/>';
-
-var_dump(10 !== '10');
+if ($today === $day) {
+    echo "Today is holiday";
+} else {
+    echo "Today is office day";
+}
