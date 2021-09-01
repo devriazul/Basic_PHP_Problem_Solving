a9e71291c4125c3417f65ccb0b441cab5adcb497
devriazul <riazul56@gmail.com>
2021-09-01 21:23:27 +0600

    Logical operator with and '&&'

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $day = 'Friday';
 $today = 'Saturday';
 
-if ($today === $day || 'Saturday') {
+if ($today === $day && 'Saturday') {
     echo "Today is holiday";
 } else {
     echo "Today is office day";
