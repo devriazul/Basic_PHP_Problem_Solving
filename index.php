671db3777cc9a6d91218c0d68c1fd964ca3c50cd
devriazul <riazul56@gmail.com>
2021-09-01 21:10:42 +0600

    Comparison

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = 'Riazul';
+$name .= ' Islam';
+
+echo $name;
+
+echo '<br/>';
+
+//comparison
+
+var_dump(10 <= 11);
+echo '<br/>';
+
+var_dump(10 == 9);
+echo '<br/>';
+
+var_dump(10 != 9);
+echo '<br/>';
+
+//data type check and value check
+
+var_dump(10 === '10');
+echo '<br/>';
+
+var_dump(10 == '10');
+echo '<br/>';
+
+var_dump(10 !== '10');
